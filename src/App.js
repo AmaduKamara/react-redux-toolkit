f254dfe48fd0c09b9ffd6857f1a364a180b60476
Amkam <amaduskamara@gmail.com>
2022-01-29 09:43:53 +0000

    Add authSlice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
 
 function App() {
   return (
-    <Counter />
+    <>
+      <Header />
+      <Auth />
+      <Counter />
+    </>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,14 @@ import { createSlice } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
 
 // 2. Create initialState
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
+
+// ################################################################################
 
 // 3. Create a slice - It needs an object with a name, initial state and reducers.
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   // Reducers is an objects - a map of all the reducers this state slice needs
   // You add methods in the object that will serve as action creators
   reducers: {
@@ -32,13 +34,37 @@ const counterSlice = createSlice({
   },
 });
 
-// 5. Call configureStore and pass a configuration object where you set a reducer property that holds a single reducer slice or many slices
-const store = configureStore({
-  reducer: counterSlice.reducer,
+// ##################################################################################
+// 7. Another slice
+const initialAuthState = {
+  isAuthenticated: false,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
 });
 
+// ###################################################################################
+// 5. Call configureStore and pass a configuration object where you set a reducer property that holds a single reducer slice or many slices
+const store = configureStore({
+  // reducer receives a configuration object with the different state properties
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
+});
+ 
 // 6. Actions - export counterSlice's actions thru counterSlice.actions
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions; 
 
 // 7. Export the store
 export default store;
