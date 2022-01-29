aed31b44045c3a9b4c17e5a3a923bdb0ba049aa1
Amkam <amaduskamara@gmail.com>
2022-01-29 10:01:32 +0000

    Add auth conditionals for components access

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </>
   );
